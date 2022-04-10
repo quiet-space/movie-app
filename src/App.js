e8adae92a4d1978275484ce0cd91a532ce063e45
quiet-space <liexil97@gmail.com>
2022-04-10 19:33:37 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+export default App;
